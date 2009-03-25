b9c09193939cd37ed7c1cc91a719b1ba35f3468a
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-03-25 01:17:42 +0000

    fixed typo

--- newsroom/settings.py
+++ newsroom/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'site.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
@@ -76,7 +76,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contri.admin',
+    'django.contrib.admin',
     'core',
     'aggregator',
     'features',
