b251fccd0cd59562bf156499f24cf28975f7f905
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-03-25 01:18:41 +0000

    fixed typos

--- newsroom/apps/core/models.py
+++ newsroom/apps/core/models.py
@@ -1,16 +1,22 @@
 
 from django.contrib.auth.models import User
-from countries import Country
+from django.db import models
+from django.contrib.localflavor.us.models import PhoneNumberField, USStateField
+from countries.models import Country
+from aggregator.models import Feed
+
 
 class Person(models.Model):
     user = models.ForeignKey(User)
     affiliate = models.ForeignKey('Affiliate')
-    middle_name =
+    middle_name = models.CharField(max_length="50")
     url = models.URLField(
             verify_exists=False,
             help_text="Your public web site.",)
+    phone = PhoneNumberField(blank=True)
     twitter_name = models.CharField(max_length="50")
     location = models.CharField(
+                blank=True,
                 max_length="100",
                 help_text="Your City and State or lat/lon.")
     
@@ -20,6 +26,7 @@ class Affiliate(models.Model):
             verify_exists=False,
             help_text="The affiliates's public web site.",)
     logo = models.ImageField(
+            blank=True,
             upload_to="affiliates",)
     city = models.CharField(max_length="100")
     state = USStateField()
