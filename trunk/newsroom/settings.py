2a576e09c6a5eed2a53f032e9fffccc388c8ea35
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-03-26 23:45:37 +0000

    added media_url to templates and titles.

--- trunk/newsroom/settings.py
+++ trunk/newsroom/settings.py
@@ -44,12 +44,12 @@ MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_DIR, 'media'))
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '4$-*-684lids1e2_r4*n3-i4pj1a=s+k!4f)k@0vruq(3ga8&b'
