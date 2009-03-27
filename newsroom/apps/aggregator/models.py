0062bbdbae7cac894f0fac12f8f4b404f5dd24b9
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-03-27 02:22:53 +0000

    fixed mysql char length

--- newsroom/apps/aggregator/models.py
+++ newsroom/apps/aggregator/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Feed(models.Model):
-    title = models.CharField(max_length=500)
+    title = models.CharField(max_length=255)
     feed_url = models.URLField(
                 unique=True, 
-                max_length=500,
+                max_length=255,
                 verify_exists=False,)
     public_url = models.URLField(
-                    max_length=500,
+                    max_length=255,
                     verify_exists=False,)
     is_defunct = models.BooleanField()
 
@@ -19,11 +19,11 @@ class Feed(models.Model):
 
 class FeedItem(models.Model):
     feed = models.ForeignKey(Feed)
-    title = models.CharField(max_length=500)
-    link = models.URLField(max_length=500)
+    title = models.CharField(max_length=255)
+    link = models.URLField(max_length=255)
     summary = models.TextField(blank=True)
     date_modified = models.DateTimeField()
-    guid = models.CharField(max_length=500, unique=True, db_index=True)
+    guid = models.CharField(max_length=255, unique=True, db_index=True)
 
     class Meta:
         db_table = 'aggregator_feeditems'
