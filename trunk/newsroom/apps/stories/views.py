e8cdf84c020d40884a41196db4c57cd6c11b4d66
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-07 19:32:33 +0000

    fixed form validation in add_story.
    added requestcontext to view. seems necessary, not sure if there is a better way.

--- trunk/newsroom/apps/stories/views.py
+++ trunk/newsroom/apps/stories/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.template import RequestContext
 from stories.forms import StoryForm
 from stories.models import Story
 
@@ -17,9 +18,13 @@ def add_story(request):
             story.author = request.user
             story.save()
             return HttpResponseRedirect(reverse('stories_story_detail',args=[story.id]))
-    form = StoryForm()
-    print form
-    return render_to_response('stories/add_story.html',locals())
+    else:
+        form = StoryForm()
+
+    return render_to_response(
+                'stories/add_story.html',
+                locals(),
+                context_instance=RequestContext(request))
     
 
 def add_page(request,story_id):
@@ -30,7 +35,10 @@ def add_page(request,story_id):
 
 def story_detail(request,story_id):
     story = get_object_or_404(Story,pk=story_id)
-    return render_to_response('stories/story_detail.html',locals())
+    return render_to_response(
+                'stories/story_detail.html',
+                locals(),
+                context_instance=RequestContext(request))
 
 
 
