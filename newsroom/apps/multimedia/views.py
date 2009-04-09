105fff8ee5ad47c48e64bdd221882f67b0c480fc
luftyluft <luftyluft@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-09 20:27:15 +0000

    minor updates to stories and multimedia

--- newsroom/apps/multimedia/views.py
+++ newsroom/apps/multimedia/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+#from photologue.models import Photo
+#from utils.response import JsonResponse, JsonErrorResponse
+
+def browse_by_type(request,media_type):
+    if "image" == media_type:
+        pass
+    elif "video" == media_type:
+        pass
+    
