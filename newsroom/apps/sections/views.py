12dafee87f1a6b5cfe6572c473a4db7e36f535ad
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-09 03:10:04 +0000

    Got edit and add working in urls.py.  Added section_add (which kind of works - it doesn't process the sectionpaths), as well as section_edit (which doesn't work) to views.py.

--- newsroom/apps/sections/views.py
+++ newsroom/apps/sections/views.py
@@ -1,34 +1,71 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from sections.models import Section
 from sections.models import SectionPath
 # from sections.forms import SectionForm
 from django.template import Context, loader
+from sections.forms import *
 
 def sections_list(request):
     sections_list = Section.objects.all()
-    t = loader.get_template('sections/sections_list.html')
-    c = Context({
-                'sections_list': sections_list,
-    })
-    return HttpResponse(t.render(c))
-
+    return render_to_response(
+            'sections/sections_list.html',
+            {'sections_list': sections_list},              
+            context_instance=RequestContext(request))
+    
 def section_detail(request, id):
     section_detail = Section.objects.get(id=id)
-    t = loader.get_template('sections/section_detail.html')
-    c = Context({
-                'section': section_detail,
-    })
-    return HttpResponse(t.render(c))
- 
-def add_section(request, **kwargs):
-    return HttpResponse('add_section')
+    return render_to_response(
+            'sections/section_detail.html',
+            {'section': section_detail},              
+            context_instance=RequestContext(request))
+    
+def section_add(request):
+    """
+    Process a new section submission.
+    """
 
-def edit_section(request, **kwargs):
-    return HttpResponse('edit_section')
+    if request.method == "POST":
+        form = SectionForm(request.POST)
+        if form.is_valid():
+            section = form.save(commit=False)
+            section.submitter = request.user
+            section.save()
+            request.user.message_set.create(
+                message='Your section has been submitted.  Thank you.')
+            return HttpResponseRedirect(request.user.get_profile().get_absolute_url())
 
+    else:
+        #form = SectionForm(user=request.user)
+        form = SectionForm()
 
+    return render_to_response(
+              'sections/section_add.html',
+              {'form':form},
+              context_instance=RequestContext(request))
 
+def section_edit(request,id):
+    """
+    Process a new section submission.
+    """
 
+    if request.method == "POST":
+        form = SectionForm(request.POST)
+        if form.is_valid():
+            section = form.save(commit=False)
+            section.submitter = request.user
+            section.save()
+            request.user.message_set.create(
+                message='Your section has been submitted.  Thank you.')
+            return HttpResponseRedirect(request.user.get_profile().get_absolute_url())
 
+    else:
+        #form = SectionForm(user=request.user)
+        form = SectionForm()
+
+    return render_to_response(
+              'sections/section_edit.html',
+              {'form':form},
+              context_instance=RequestContext(request))
