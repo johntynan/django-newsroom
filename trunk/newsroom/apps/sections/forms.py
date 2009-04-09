609a905a036d23e582386b2c255e59caf36ec959
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-09 03:15:24 +0000

    forgot to add forms.py to the last commit

---
+++ trunk/newsroom/apps/sections/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from sections.models import Section
+from sections.models import SectionPath
+
+class SectionForm(forms.ModelForm):
+    
+    class Meta:
+        model = Section
+        fields = ('title','slug','description','collection')
