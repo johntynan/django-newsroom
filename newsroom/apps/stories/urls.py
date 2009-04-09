4632024eab5971c377f70946f0d955365c7b64af
luftyluft <luftyluft@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-09 23:32:54 +0000

    got basic media add working in edit page for videos

--- newsroom/apps/stories/urls.py
+++ newsroom/apps/stories/urls.py
@@ -4,8 +4,8 @@ urlpatterns = patterns('stories.views',
     url('^story/add/$','add_story',name='stories_add_story'),
     url('^story/(?P<story_id>\d+)/edit/$','edit_story',name='stories_edit_story'),
     
-    url('^story/page/(?P<page_id>\d+)/edit/$','edit_page',name='stories_edit_page'),
-    
+    url(r'^story/page/(?P<page_id>\d+)/edit/$','edit_page',name='stories_edit_page'),
+    url(r'^story/page/(?P<page_id>\d+)/media/add/$','page_add_media',name='stories_page_add_media'),
     url('^(?P<slug>[\d\w-]+)/$','show_story',name="stories_show_story"),
 )
 
