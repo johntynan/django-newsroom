456884caff972830074334fad8117f07de894e2e
luftyluft <luftyluft@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-04-11 21:18:32 +0000

    pushed the interface further along for stories; working on the media screens; general cleanup/refactoring; added attribution and site attributes to Media

--- trunk/newsroom/apps/stories/urls.py
+++ trunk/newsroom/apps/stories/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('stories.views',
     url('^story/(?P<story_id>\d+)/edit/$','edit_story',name='stories_edit_story'),
     #story overview (page list)
     url('^story/(?P<story_id>\d+)/pages/$','story_pages',name='stories_story_pages'),
+    #story media overview (media list)
+    url('^story/(?P<story_id>\d+)/media/$','story_media',name='stories_story_media'),
     #add page
     url(r'^story/(?P<story_id>\d+)/page/add/$','add_page',name='stories_add_page'),
     #edit page
