eb5ab670566e0e0c14a8797d0de628bb90a6863e
mlouro <mlouro@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-07 17:06:05 +0000

    minor tweak to lightbox files

--- branches/pinax/newsroom_project/media/js/lightbox/thickbox.js
+++ branches/pinax/newsroom_project/media/js/lightbox/thickbox.js
@@ -5,7 +5,7 @@
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
 */
 
-var tb_pathToImage = "/site_media/js/thickbox/loadingAnimation.gif";
+var tb_pathToImage = "/site_media/js/lightbox/loadingAnimation.gif";
 
 /*!!!!!!!!!!!!!!!!! edit below this line at your own risk !!!!!!!!!!!!!!!!!!!!!!!*/
 
