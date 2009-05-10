8640ee046d5717dfbabd7d46a696d37632be56d3
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-10 21:12:16 +0000

    fixed image edit.

--- branches/pinax/newsroom_project/apps/photos/views.py
+++ branches/pinax/newsroom_project/apps/photos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
-from photos.forms import PhotoForm, PhotoImageForm
+from photos.forms import PhotoForm, ImageForm
 from photos.models import Photo
 
 def photo_edit(request, photo_id):
@@ -13,7 +13,7 @@ def photo_edit(request, photo_id):
         form = PhotoForm(request.POST,request.FILES, instance=photo)
         # don't include instance, only process the POST/FILES data
         # for the image form
-        image_form = PhotoImageForm(
+        image_form = ImageForm(
                         request.POST, request.FILES,instance=photo.image)
         if form.is_valid() and image_form.is_valid():
             photo = form.save(commit=False)
@@ -21,6 +21,7 @@ def photo_edit(request, photo_id):
             photo.slug = slugify(photo.title)
             image = image_form.save()
             old_image = None
+
             if image != photo.image:
                 old_image = photo.image
                 photo.image = image
@@ -29,7 +30,6 @@ def photo_edit(request, photo_id):
             form.save_m2m()
 
             if old_image:
-                print 'try deleting old image'
                 old_image.image.delete()
                 old_image.delete()
 
@@ -37,8 +37,8 @@ def photo_edit(request, photo_id):
                         message='Your photo was saved.')            
             return HttpResponseRedirect(reverse('multimedia_browse'))
     else:
-        form = VideoForm(instance=photo)
-        image_form = VideoFrameForm(instance=photo.image)
+        form = PhotoForm(instance=photo)
+        image_form = ImageForm(instance=photo.image)
 
     return render_to_response('photos/photo_edit.html',locals(),context_instance=RequestContext(request))
 
