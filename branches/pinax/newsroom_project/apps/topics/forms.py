a51223dcfa5492aea109e4f0b27e3a28ef3489f3
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-17 00:55:40 +0000

    changed collection property to topic_path, added the ability to add a photo to a topic

--- branches/pinax/newsroom_project/apps/topics/forms.py
+++ branches/pinax/newsroom_project/apps/topics/forms.py
@@ -1,14 +1,21 @@
 from django import forms
 from topics.models import Topic
 from topics.models import TopicPath
+from photos.models import Photo
 
 class TopicForm(forms.ModelForm):
     
     class Meta:
         model = Topic
-        fields = ('title','slug','description','collection')
+        fields = ('title','slug','description','topic_path')
 
 class TopicPathForm(forms.ModelForm):
     
     class Meta:
         model = TopicPath
+
+class TopicImageForm(forms.ModelForm):
+
+    class Meta:
+        model = Photo
+        fields = ('title','image','caption','promo')
