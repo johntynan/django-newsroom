bec9977b38d0bf90c5b27fb03fb125894451a475
yann.malet <yann.malet@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-19 21:04:11 +0000

    Rework the Story Model to add the capability to relate any object in the database. This change will require a schema update. The relation between Story and Project has been removed as well as the direct relation between Story and Media.

--- newsroom_project/apps/stories/admin.py
+++ newsroom_project/apps/stories/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from stories.models import Story,Page
+from stories.models import Story, RelatedContent, Page
+
+class RelatedContentInline(admin.StackedInline):
+    model = RelatedContent
+
+class PageInline(admin.StackedInline):
+    model = Page
 
 class StoryAdmin(admin.ModelAdmin):
     search_fields = ['headline','slug','summary']
     prepopulated_fields = {'slug': ('headline',)}
 
+    list_display = ('headline','creation_date',
+        'modification_date' )
+    list_display_links = ('headline',)
+    list_filter = ('status',)
+    filter_horzontal = ('sites',)
+
+    inlines = (RelatedContentInline, PageInline)
+
 admin.site.register(Story,StoryAdmin)
-admin.site.register(Page)
 
 
