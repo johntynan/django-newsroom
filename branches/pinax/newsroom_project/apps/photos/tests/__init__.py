d21570bd2fa2d58649eb1c127c1a66e89faf65fc
yann.malet <yann.malet@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-20 21:40:47 +0000

    Add a test to check that the view adding a Photo is working. Add a test_image.jpg this file is used by that test.

--- branches/pinax/newsroom_project/apps/photos/tests/__init__.py
+++ branches/pinax/newsroom_project/apps/photos/tests/__init__.py
@@ -1,3 +1,6 @@
+from photos.models import Photo
+import os.path
+import os
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -29,10 +32,32 @@ class PhotoUrlTests(TestCase):
     def tearDown(self):
         self.client.logout()
 
-
     def test_add_photo_get_url(self):
         self.response = self.client.get(
             reverse("stories_story_add_media",
             kwargs={"story_id" : self.story.id,
                 "media_type" : "photo"}))
         self.assertEqual(self.response.status_code, 200)
+
+    def test_add_photo_post_url(self):
+        try:
+            TESTS_DIR = os.path.abspath(os.path.dirname(__file__))
+            photo = open(os.path.join(TESTS_DIR, "test_image.jpg"))
+            self.response = self.client.post(
+                reverse("stories_story_add_media",
+                kwargs={"story_id" : self.story.id,
+                    "media_type" : "photo"}),
+                    {
+                        "title":"test picture",
+                        "status":"D",
+                        "license":"http://creativecommons.org/licenses/by/2.0/",
+                         "authors" : (self.user.id,),
+                         "sites" : (1,),
+                         "image":photo,
+                    })
+        finally:
+            photo.close()
+        
+        self.assertEqual(self.response.status_code, 302)
+        # Check if the photo as been created
+        self.assertEqual(1, Photo.objects.count())
