60033c56544de9d4bd516668be48f3c0df1ab433
mandric <mandric@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-05-27 04:39:05 +0000

    added news21 main site using esser theme.

---
+++ branches/pinax/news21_com/urls.py
@@ -0,0 +1,36 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.conf import settings
+from account.openid_consumer import PinaxConsumer
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^asu_news21/', include('asu_news21.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {'template': 'front.html'}),
+    (r'^topics/$', direct_to_template, {'template': 'topics.html'}),
+    (r'^openid/(.*)', PinaxConsumer()),
+    (r'^stories/', include('stories.urls.publication')),
+    (r'^profiles/', include('basic_profiles.urls')),
+    (r'^topics/',include('topics.urls')),
+    (r'^account/', include('account.urls')),
+    (r'^photos/',include('photos.urls')),
+    (r'^videos/',include('videos.urls')),
+    (r'^about/',include('about.urls')),
+    (r'^promos/',include('promos.urls')),
+)
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('', 
+        (r'^site_media/(?P<path>.*)$', 'staticfiles.views.serve')
+    )
