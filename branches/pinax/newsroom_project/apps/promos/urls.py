ae2b0fac8f23ea9c6662d4b89c374d530b198f4a
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-06-01 12:48:44 +0000

    added ability to see promos on a map.  ex: http://localhost:8000/promos/kml_feeds_map/all/

--- branches/pinax/newsroom_project/apps/promos/urls.py
+++ branches/pinax/newsroom_project/apps/promos/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 from geotags.models import Point
 from geotags.forms import PointForm
 
+from geotags.views import kml_feed, kml_feed_map, kml_feeds_map
+
 urlpatterns = patterns('promos.views',
 
     url(r'^$',
@@ -31,5 +33,22 @@ urlpatterns = patterns('promos.views',
         {"form_class":PointForm, "geotag_class":Point,
          "template":"promos/promo_add_edit_point.html"}
         , name="promos_promo_add_edit_point"),
+    
+    # KML feeds
+    url(r'^kml_feed/(?P<geotag_class_name>[a-z]+)/$',kml_feed,
+        name="geotags-kml_feed"),
+    url(r'^kml_feed/(?P<geotag_class_name>[a-z]+)/(?P<content_type_name>[a-z ]+)/$',kml_feed,
+        name="geotags-kml_feed_per_contenttype"),
+
+    # KML Feeds visualiser
+    url(r'^kml_feeds_map/all/$', kml_feeds_map,
+        name="geotags-kml_feeds_map"),
+    url(r'^kml_feeds_map/all/(?P<content_type_name>[a-z]+)/$', kml_feeds_map,
+        name="geotags-kml_feeds_map_per_contenttype"),
+
+    url(r'^kml_feed_map/(?P<geotag_class_name>[a-z]+)/$', kml_feed_map,
+        name="geotags-kml_feed_map"),
+    url(r'^kml_feed_map/(?P<geotag_class_name>[a-z]+)/(?P<content_type_name>[a-z ]+)/$', kml_feed_map,
+        name="geotags-kml_feed_map_per_contenttype"),
     
 )
