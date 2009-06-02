dbff24382d9f5943f069e3ab989464f6b33cfcc9
yann.malet <yann.malet@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-06-02 20:17:16 +0000

    change the default start tag /end tag for inlines. Modify the template used by modelInline for Photo we can now path witdth and height as argument. In order to see this working you need to add something like <% photo 1 width=100 height=100 %> this will be transformed into <img src='path to the picture' style='width:100px' height='100px'; />

--- newsroom_project/settings.py
+++ newsroom_project/settings.py
@@ -201,6 +201,9 @@ POSTGIS_TEMPLATE='template_postgis'
 # http://bazaar.launchpad.net/%7Epopulous/django-geotagging/geotags/annotate/head%3A/geotagging_demo_project/geoip_datasets/get_geoip_datasets.sh
 GEOIP_PATH=PROJECT_ROOT
 
+# django_inlines
+INLINES_START_TAG = '<%'
+INLINES_END_TAG = '%>'
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
