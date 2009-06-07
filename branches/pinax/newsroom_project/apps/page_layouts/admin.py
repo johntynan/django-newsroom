d9995b69bd91a1c49d08a11f1efb2bb55a33c0ec
mlouro <mlouro@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-06-07 10:58:35 +0000

    removed unecessary inlineModel for ImageWidgets on pageLayouts

--- branches/pinax/newsroom_project/apps/page_layouts/admin.py
+++ branches/pinax/newsroom_project/apps/page_layouts/admin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from page_layouts.models import PageLayout, TextWidget, ImageWidget
+from page_layouts.models import PageLayout, TextWidget
 
 admin.site.register(PageLayout)
 admin.site.register(TextWidget)
-admin.site.register(ImageWidget)
