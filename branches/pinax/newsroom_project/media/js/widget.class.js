636e4de7449a801a5df0a72baeb1eac40f7696ea
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-07-06 04:06:16 +0000

    enabled most of the expected visual editor controls along with the ability to embed google maps etc.

--- branches/pinax/newsroom_project/media/js/widget.class.js
+++ branches/pinax/newsroom_project/media/js/widget.class.js
@@ -152,15 +152,18 @@ var widget = {
 
             // Theme options
             //theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect",
-            theme_advanced_buttons1 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,code",
-            //theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,help,code",
-            theme_advanced_buttons2 : "",
-            //theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,fullscreen",
-            theme_advanced_buttons3 : "",
+            theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect",
+            theme_advanced_buttons2 : "cut,copy,paste,pastetext,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,code",
+            //theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,help,code",
+            //theme_advanced_buttons2 : "",
+            theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,fullscreen",
+            // theme_advanced_buttons3 : "",
             theme_advanced_toolbar_location : "top",
             theme_advanced_toolbar_align : "left",
             theme_advanced_statusbar_location : "bottom",
             theme_advanced_resizing : false,
+            invalid_elements: "",
+            extended_valid_elements : "object,applet,script,iframe[src|width|height]",
             /*
             save_callback : "widget.save_text_editor",
             */
