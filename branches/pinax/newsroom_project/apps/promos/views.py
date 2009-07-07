258c6828b08a70b240ab378d7a622a220b6409c0
yann.malet <yann.malet@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-07-07 21:15:07 +0000

    Adds check to make sure that the promo is equal to billboard to promo

--- branches/pinax/newsroom_project/apps/promos/views.py
+++ branches/pinax/newsroom_project/apps/promos/views.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -319,6 +319,8 @@ def promo_billboard_detail(request, promo_id, billboard_id):
 
     user_billboards = user_objects_qs(PromoBillboard, request.user)
     billboard = get_object_or_404(user_billboards, pk=billboard_id)
+    if promo != billboard.promo:
+        raise Http404()  
 
     return render_to_response(
             'promos/promo_billboard_detail.html',{
