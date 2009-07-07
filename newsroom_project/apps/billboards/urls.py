1afc6b11040148e8e9a12fb5f13c39ebed3fb704
jgtynan <jgtynan@8e7a9df6-a9d9-11dd-9f94-e5dfbee8a4f1>
2009-07-07 01:21:05 +0000

    fixed errors added templates

--- newsroom_project/apps/billboards/urls.py
+++ newsroom_project/apps/billboards/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import *
 from billboards.models import Billboard
 
-urlpatterns = patterns('billboard.views',
+urlpatterns = patterns('billboards.views',
     url(r'^$',
         'billboard_list',
-        name="billboard_billboard_list"),
+        name="billboards_billboard_list"),
     url(r'^(?P<id>\d+)/$', 
         'billboard_detail',
-        name='billboard_billboard_detail'),
+        name='billboards_billboard_detail'),
     url(r'^add/$',
         'billboard_add',
-        name='billboard_billboard_add'),
+        name='billboards_billboard_add'),
 )
